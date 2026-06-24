e26b439f8cf98fe9a5e0db9c83d82c64720d3d12
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2026-06-24 13:39:35 -0300

    Add support for lazy loading on Icon component

--- apps/frontend/src/components/ui/Icon.tsx
+++ apps/frontend/src/components/ui/Icon.tsx
@@ -1,10 +1,11 @@
 import { tw } from "@/utils/tw.js"
-import { Props, rawHtml } from "@jsxrx/core"
-import { map, Observable } from "rxjs"
+import { Props, rawHtml, Suspense } from "@jsxrx/core"
+import { from, map, Observable, of, switchMap } from "rxjs"
+import Skeleton from "./Skeleton.js"
 
 export type IconProps = Readonly<{
   id: string
-  content: string
+  content: string | Promise<string | { default: string }> | { default: string }
   className?: string
 }>
 
@@ -15,7 +16,23 @@ export default function Icon(props$: Observable<IconProps>) {
     <div
       className={tw("w-full h-full [&>svg]:w-full [&>svg]:h-full", className$)}
     >
-      {id$.pipe(map(id => rawHtml(id, content$)))}
+      <Suspense fallback={<Skeleton />}>
+        {id$.pipe(
+          map(id =>
+            rawHtml(
+              id,
+              content$.pipe(
+                switchMap(content =>
+                  content instanceof Promise ? from(content) : of(content),
+                ),
+                map(content =>
+                  typeof content === "string" ? content : content.default,
+                ),
+              ),
+            ),
+          ),
+        )}
+      </Suspense>
     </div>
   )
 }
